a7b6408e8e1af5e2589c40ba758ab84798cf1abf
Sean D'Epagnier <seandepagnier@gmail.com>
2016-07-31 11:44:29 -0400

    update format

--- yamlgenerator.c
+++ yamlgenerator.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+struct box
+{
+    char name[256];
+    double lat_min, lon_min, lat_max, lon_max;
+    struct box *next;
+};
+
+struct box *boxes[20];
+
 int main(int argc, char **argv)
 {
     if(argc < 2) {
@@ -12,57 +21,54 @@ int main(int argc, char **argv)
 
     char *line = 0;
     size_t n;
-    char scale;
     int index, id;
-    double lat_min, lon_min, lat_max, lon_max;
     int ind = 0;
 
-    FILE *c = tmpfile();
-    printf("seeds:\n");
-    printf("  level%d:\n", level);
-//    printf("    caches: [level%d_mbtiles]\n", level);
-    printf("    caches: [default]\n");
-    printf("    coverages: [");
 
-    fprintf(c, "coverages:\n");
     while(getline(&line, &n, stdin) != EOF) {
-        char n[1024];
-        int ret = sscanf(line, "%c%s %lf %lf %lf %lf\n", &scale, n,
-                         &lat_min, &lon_min, &lat_max, &lon_max);
+        struct box *b = malloc(sizeof(struct box));
+        int level;
+        int ret = sscanf(line, "%s %d %lf %lf %lf %lf\n", b->name, &level,
+                         &b->lat_min, &b->lon_min, &b->lat_max, &b->lon_max);
         free(line);
         line = 0;
 
-        if(ret != 6)
+        if(ret != 6 || level < 0 || level > 20) {
             fprintf(stderr, "invalid line %d (%d): %s\n", ind, ret, line);
-        else {
-            char name[1024];
-            sprintf(name, "%s%c", n, scale);
-//            int l = 10 + scale - 'A';
-
-            //          if(level != l)
-//                continue;
-
-            printf("%s_coverage,", name);
-
-            fprintf(c, "  %s_coverage:\n", name);
-            fprintf(c, "    bbox: [%f, %f, %f, %f]\n", lon_min, lat_min, lon_max, lat_max);
-            fprintf(c, "    srs: \"EPSG:4326\"\n");
-
-            fprintf(c, "\n");
+            return 1;
+        } else {
+            b->next = boxes[level];
+            boxes[level] = b;
         }
         ind++;
     }
-    printf("]\n");
-    printf("    levels:\n");
-    printf("      from: %d\n", level);
-    printf("      to: %d\n", level);
-    printf("\n");
 
+    printf("seeds:\n");
+    
+    for(int level = 0; level<20; level++) {
+        if(!boxes[level])
+            continue;
 
-    printf("\n\n");
-    int ch;
-    fseek(c, 0, SEEK_SET);
-    while((ch = fgetc(c)) != EOF)
-        fputc(ch, stdout);
+        printf("  level%d:\n", level);
+        printf("    caches: [default]\n");
+        printf("    coverages: [");
+        for(struct box *b = boxes[level]; b; b=b->next)
+            printf("%s_%d_coverage,", b->name, level);
+        printf("]\n");
+        printf("    levels:\n");
+        printf("      from: %d\n", level);
+        printf("      to: %d\n", level);
+        printf("\n");
+    }
+    
+    printf("coverages:\n");
+    for(int level = 0; level<20; level++) {
+        for(struct box *b = boxes[level]; b; b=b->next) {
+            printf("  %s_%d_coverage:\n", b->name, level);
+            printf("    bbox: [%f, %f, %f, %f]\n", b->lon_min, b->lat_min, b->lon_max, b->lat_max);
+            printf("    srs: \"EPSG:4326\"\n");
+        }
+    }
     printf("\n");
+    return 0;
 }
